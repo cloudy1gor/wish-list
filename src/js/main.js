58341572fe73e655fc779f921cd4d54770d3112b
Ihor Savelyev <savelyev7979@gmail.com>
2021-04-11 20:34:47 +0300

    add new section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {});
+// core version + navigation, pagination modules:
+import Swiper from "swiper";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // init Swiper:
+  const swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+  });
+});
